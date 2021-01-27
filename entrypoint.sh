9257891329d0cbd4d62d35a55d9543622f8663ae
luismiguelsaez <luismiguelsaez83@gmail.com>
2021-01-27 17:49:34 +0100

    Added environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,26 @@
+#!/bin/bash
 
-if [ -d /certificates ] && [ -f /certificates/*.pem ]
+CERTS_DIR=${CERTS_DIR:-"/certificates"}
+STORE_DIR=${STORE_DIR:-"/keystore"}
+STORE_NAME=${STORE_NAME:-"keystore.jks"}
+STORE_PASS=${STORE_PASS:-"changeit"}
+CACERTS=1
+
+if [ $CACERTS -eq 0 ]
+then
+    PARAMS="-keystore ${STORE_DIR}/${STORE_NAME}"
+else
+    PARAMS="-trustcacerts -cacerts"
+fi
+ 
+if [ -d ${CERTS_DIR} ] && [ $(ls ${CERTS_DIR}/*.pem 2>/dev/nul | wc -l ) -gt 0 ]
 then
     c=0
-    for CERT in $(ls /certificates/*.pem)
+    for CERT in $(ls ${CERTS_DIR}/*.pem)
     do
         c=$(( $c + 1 ))
-        keytool -noprompt -trustcacerts -cacerts -storepass changeit -import -alias auto-add-$c -file $CERT
+        keytool -noprompt ${PARAMS} -storepass ${STORE_PASS:-changeit} -import -alias auto-add-$c -file $CERT
     done
 else
-    echo "No certificate files found in directory"
+    echo "No certificate files found in directory ${CERTS_DIR}"
 fi
